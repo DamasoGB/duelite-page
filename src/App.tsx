7a307f8de3e47ede39c0b6bf3bc2d3d894b0d2a4
Damaso Garcia <dimnas12@gmail.com>
2024-04-21 02:11:42 +0200

    image slider done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Dispatch, SetStateAction, useState } from "react";
 import { Header } from "./components/Header";
 import { GameDesc } from "./components/GameDesc";
 import { SteamLink } from "./components/SteamLink";
+import { ImageSlider } from "./components/ImageSlider";
+import { IMAGES } from "./assets/data";
 import "./App.css";
-import { Dispatch, SetStateAction, useState } from "react";
 
 interface ILanguage {
   language: string;
@@ -17,6 +19,7 @@ export function App() {
       <Header setLanguage={setLanguage} />
       <GameDesc language={language} />
       <SteamLink language={language} />
+      <ImageSlider images={IMAGES} />
     </>
   );
 }
---
+++ src/assets/data.ts
@@ -0,0 +1,14 @@
+export const IMAGES = [
+    {
+        url: "src/assets/imagePlaceholder1.jpg",
+        alt: "Image 1"
+    },
+    {
+        url: "src/assets/imagePlaceholder2.jpg",
+        alt: "Image 2"
+    },
+    {
+        url: "src/assets/imagePlaceholder3.png",
+        alt: "Image 3"
+    },
+]
