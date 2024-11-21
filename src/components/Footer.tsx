92d0e0ed93f2ed68fbf9976ed8a8903a81a571a4
DamasoGB <dimnas12@gmail.com>
2024-11-21 16:26:26 +0100

    twitter change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ return(
     <footer>
         <a href="https://store.steampowered.com/curator/45097743" className='footerlink' aria-label="See what games have BetakingStudios done"><img src={Steam} className='footerimg' alt='Steam'/></a>
         <a href="https://discord.gg/c46PUqhxqp" className='footerlink' aria-label='Join the official Discord'><img src={Discord} className='footerimg' alt='Discord'/></a>
-        <a href="https://twitter.com/duelitegame" className='footerlink' aria-label='See what BetakingStudios have posted on Twitter'><img src={Twitter} className='footerimg' alt='Twitter'/></a>
+        <a href="https://twitter.com/betakingstudios" className='footerlink' aria-label='See what BetakingStudios have posted on Twitter'><img src={Twitter} className='footerimg' alt='Twitter'/></a>
         <a href="mailto:betakingstudios@gmail.com" className='footerlink' aria-label='Contact us'><img src={Mail} className='footerimg' alt='Mail'/></a>
     </footer>
 )
