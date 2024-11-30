da76d51978b98001509657d0cea357b31bc58bea
DamasoGB <88084742+DamasoGB@users.noreply.github.com>
2024-11-30 15:50:30 +0100

    Add Rabbit trail

--- src/data/steamLinksData.ts
+++ src/data/steamLinksData.ts
@@ -3,8 +3,8 @@ export const steamLinksData = [
       title: "Duelite",
       src: "https://store.steampowered.com/widget/2961480/"
     },
-    // {
-    //   title: "Otro Juego",
-    //   src: "https://store.steampowered.com/widget/2933620/"
-    // },
+    {
+      title: "Rabbit Trail",
+      src: "https://store.steampowered.com/widget/3371710/"
+    },
 ];
